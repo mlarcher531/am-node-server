4834ff8c70878f2d3a38f3f9b9cb9ede5bba333e
mlarcher531 <mlarcher531@gmail.com>
2021-03-02 09:45:28 -0600

    fs store

--- app.js
+++ app.js
@@ -11,18 +11,29 @@ const server = http.createServer((req, res) => {
         res.write('</html>')
         return res.end()  
     }
+
     if (url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message);
+        })
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end()
     }
+
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
     res.write('<body><h1>This is weird</h1></body>')
     res.write('</html>')
     res.end()
+
 })
 
 server.listen(3000)
