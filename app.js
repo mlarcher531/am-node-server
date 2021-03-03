12b1eb001e8393af492b73298547c45274e81d81
mlarcher531 <mlarcher531@gmail.com>
2021-03-03 10:51:09 -0600

    moved response logic

--- app.js
+++ app.js
@@ -4,27 +4,28 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
     const url = req.url
     const method = req.method
-    if (url === '/'){
+    if (url === '/') {
         res.write('<html>')
         res.write('<head><title>Enter Info</title></head>')
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
         res.write('</html>')
-        return res.end()  
+        return res.end()
     }
 
-    if (url === '/message' && method === 'POST'){
+    if (url === '/message' && method === 'POST') {
         const body = []
         req.on('data', (chunk) => {
             body.push(chunk)
         })
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString()
             const message = parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end()
+            });
         })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end()
     }
 
     res.setHeader('Content-Type', 'text/html')
