f79e83a8c14e9e888f968bd876e965ce4e4ba13e
mlarcher531 <mlarcher531@gmail.com>
2021-03-11 11:23:45 -0600

    basic views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -8,7 +9,11 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false})) //will not parse JSON and other file types
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
     // console.log('in home / !')
-    res.send('<h1>Hello my friend</h1>') // ends request, does not advance like with next()
+    res.sendFile(path.join(__dirname, '../', 'views' , 'shop.html'))
 })
 
 module.exports = router
