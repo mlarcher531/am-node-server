e512db86e2360afd68f503d8303759622bbbdb8f
mlarcher531 <mlarcher531@gmail.com>
2021-03-11 09:58:37 -0600

    middleware

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http')
-const routes = require('./routes')
+const express = require('express')
 
-const server = http.createServer(routes)
+const app = express()
 
-server.listen(3000)
+app.use((req, res, next) => {
+    console.log('in the middleware!')
+    next() // allows request to ocntinue to next middleware
+})
+
+app.use((req, res, next) => {
+    console.log('in ANOTHER middleware!')
+    res.send('<h1>Hello my friend</h1>') // ends request, does not advance like with next()
+})
+
+app.listen(3000)
