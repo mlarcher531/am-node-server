9215c24ced2c8a18408f52796fba267e80d33ab2
mlarcher531 <mlarcher531@gmail.com>
2021-03-18 18:40:11 -0500

    all pug with layout and active page

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
     const products = adminData.products
-    res.render('shop', {prods: products, docTitle: 'My Shop'})
+    res.render('shop', {prods: products, docTitle: 'My Shop', path: '/'})
 })
 
 module.exports = router
